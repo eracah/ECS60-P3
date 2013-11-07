73a8f6501a6a0d69d6ddf31deb0e5371172f93a1
Evan Racah <ejracah@gmail.com>
2013-11-06 20:21:49 -0800

    Added extendible leaf implementation

---
+++ ExtendibleLeaf.cpp
@@ -0,0 +1,21 @@
+//Evan Racah
+//Extendible leaf implementations
+
+
+	ExtendibleLeaf::ExtendibleLeaf(int LeafSize, int leadingBits)
+	{
+
+	}//ExtendibleLeaf::ExtendibleLeaf
+	ExtendibleLeaf* ExtendibleLeaf::insert(const int &object)
+	{
+		
+	}
+	void ExtendibleLeaf::remove(const int &object);
+  const int & ExtendibleLeaf::find(const int &object);
+  void ExtendibleLeaf::split(const int &object);
+  void ExtendibleLeaf::setLeadingBits(int leadBits); //private?
+	const int & ExtendibleLeaf::getFirstElement();
+
+
+
+
