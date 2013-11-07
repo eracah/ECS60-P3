b859d548db09156f9f477b19b3d1f1974fc64d1d
Evan Racah <ejracah@gmail.com>
2013-11-06 22:51:11 -0800

    Added a notfound and a findIndex

--- ExtendibleLeaf.h
+++ ExtendibleLeaf.h
@@ -1,8 +1,9 @@
 //Evan Racah
 //ExtendibleLeaf 
-
+#ifndef EXTENDIBLE_LEAF_H
+  #define EXTENDIBLE_LEAF_H
 using namespace std;
-#include "vector.h" //include after all other includes
+#include <vector> //include after all other includes
 class ExtendibleLeaf
 {
 public:
@@ -10,12 +11,16 @@ public:
 	ExtendibleLeaf* insert(const int &object);
 	void remove(const int &object);
   const int & find(const int &object);
-  void split(const int &object);
+  ExtendibleLeaf* split(const int &object);
   void setLeadingBits(int leadBits); //private?
   int getLeadingBits() 
   {
   	return leadingBits;
   } //get the number of leading bits
+  int getSize()
+  {
+  	return currentSize;
+  }
   const int & getFirstElement(); //returns first element in vector so calling object has an idea of whats in there
   //get a an element from a leaf or just the hashed leading bits of element in leaf?
 
@@ -25,5 +30,10 @@ private:
 	int currentSize;
 	int leadingBits; // the maximum number of leading bits that a leaf has in common
 	//Extendibleleaf parent or way of seeing old hash key to leaf?
+	int findIndex(const int &object);
+	int notFound;
 };
 
+
+#include "ExtendibleLeaf.cpp"
+#endif
