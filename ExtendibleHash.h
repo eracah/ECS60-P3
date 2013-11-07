f6934070dd60e63e4eba8558037ba89236ebf60f
Evan Racah <ejracah@gmail.com>
2013-11-06 20:23:15 -0800

    added a few comments

--- ExtendibleHash.h
+++ ExtendibleHash.h
@@ -1,10 +1,12 @@
 #ifndef EXTENDIBLE_HASH_H
   #define EXTENDIBLE_HASH_H
 
+#include "ExtendibleLeaf.h"
+#include "vector"
 
 class ExtendibleHash
 {
-//  ExtendibleLeaf  **Directory;
+  vector<ExtendibleLeaf *> Directory;
   int bits;
   int size;
   int LeafSize;
@@ -13,8 +15,8 @@ public:
   ExtendibleHash(const int & notFound, int s, int LSize = 2);
   void insert(const int &object);
   void remove(const int &object);
-  const int & find(const int &object); //why the refrence here? save from copying?
-  void split(const int &object);   
+  const int & find(const int &object); //why the reference here? save from copying?
+  void split(const int &object);   //resizes vector?????????
   int GetBits()const {return bits;}
 
 }; // class ExtendibleHashing
